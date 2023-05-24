f433f7886394c2dbfe69d2ff7179a945e6254eaf
DEINER007 <deinerospino01@gmail.com>
2023-05-24 16:20:21 -0500

    Update prueba_13.java

--- prueba_13.java
+++ prueba_13.java
@@ -1,3 +1,12 @@
 public class prueba_13 {
-    
+    public static void main(String [] args) {
+        int [] array = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        for (int i = 0; i < array.length; i++) {
+            System.out.println (array [i] + ",");
+        }
+        System.out.println ("Incremtamos en uno.");
+        for (int i = 0; i < array.length; i++) {
+            array [i] ++;
+        }
+    }
 }
