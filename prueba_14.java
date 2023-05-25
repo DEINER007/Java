e947bbb5dc497eb9dfd80074bccb9081e4d5888e
DEINER007 <deinerospino01@gmail.com>
2023-05-24 21:08:09 -0500

    Update prueba_14.java

--- prueba_14.java
+++ prueba_14.java
@@ -1,3 +1,45 @@
+import java.util.Scanner;
 public class prueba_14 {
-    
+    static int [] array = new int [5];
+    static Scanner SC  = new Scanner (System.in);
+    public static void main (String [] args) {
+        insertarDatosArray ();
+        sumarArray ();
+        maxArray ();
+        promedioArray ();
+    }
+    private static void insertarDatosArray () {
+        for (int i = 0; i < array.length; i++) {
+            System.out.print ("Insertar array [" + i + "]= ");
+            array [i] = SC.nextInt ();
+        }
+    }
+    private static void sumarArray () {
+        System.out.print ("Sumar= ");
+        int acomulador = 0;
+        for (int i = 0; i < array.length; i++) {
+            acomulador = acomulador + array [i];
+            System.out.print (" + " + array [i]);
+        }
+        System.out.println (" = " + acomulador);
+    }
+    private static void maxArray () {
+        System.out.print ("Maximo= ");
+        int max = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array [i] > max) {
+                max = array [i];
+            }
+        }
+        System.out.println (max);
+    }
+    private static void promedioArray () {
+        System.out.print ("Promedio= ");
+        double promedio = 0;
+        for (int i = 0; i < array.length; i++) {
+            promedio = promedio + array [i];
+        }
+        promedio = promedio / array.length;
+        System.out.println (promedio);
+    }
 }
